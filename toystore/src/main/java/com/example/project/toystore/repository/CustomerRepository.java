09bda4897614187d16952e1bc58a00b543328c48
Shamithasivaa <128037182+Shamithasivaa@users.noreply.github.com>
2023-09-01 14:13:05 +0530

    Delete toystore directory

--- toystore/src/main/java/com/example/project/toystore/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.toystore.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.project.toystore.model.Customer;
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer>{
-	
-}
--- toystore/src/main/java/com/example/project/toystore/repository/ToyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.toystore.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.project.toystore.model.Toy;
-@Repository
-public interface ToyRepository extends JpaRepository<Toy,Integer>{
-
-}
